6f209b3c9e320f0da61cb0c80b82c79a4a214e60
= <=>
2023-02-14 14:05:36 +0000

    5- Faire un service web qui permet de crée uncompte chez un service de transfert.

---
+++ src/main/java/com/iscae/projetdevoir/services/CreateAccountService.java
@@ -0,0 +1,40 @@
+package com.iscae.projetdevoir.services;
+
+import com.iscae.projetdevoir.models.Account;
+import com.iscae.projetdevoir.models.TransferService;
+import com.iscae.projetdevoir.utils.DataStorage;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("account")
+public class CreateAccountService {
+
+    private DataStorage dataStorage = DataStorage.getInstance();
+
+    @POST
+    @Path("create")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createAccount(
+            @QueryParam("transferService") String transferServiceName) {
+
+        // Validate the information
+        if (transferServiceName == null || transferServiceName.isEmpty()) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("Transfer service is required")
+                    .build();
+        }
+
+        TransferService transferService = dataStorage.getTransferService(transferServiceName);
+        Account account  = new Account(transferService);
+        dataStorage.addAccount(account);
+
+        // Return success status
+        return Response.ok().entity("Account created successfully").build();
+    }
+}
+
